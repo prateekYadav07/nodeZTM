598c035f50cdf226ed8aab3856f10bf134445f52
prateekYadav07 <yadavprateek39@gmail.com>
2023-04-05 23:32:21 +0530

    request factoring - not recommended approach

--- HTTP-SERVER/index.js
+++ HTTP-SERVER/index.js
@@ -2,28 +2,39 @@ const http = require("http");
 
 const PORT = 3000;
 
+const supes = [
+  {
+    id: 0,
+    name: "Bruce",
+  },
+  {
+    id: 1,
+    name: "Diana",
+  },
+  {
+    id: 3,
+    name: "Clark",
+  },
+];
+
 const server = http.createServer((req, res) => {
-  if (req.url === "/batman") {
-    // res.writeHead(200, "Request is succesful with 200", {
-    //   // "Content-Type": "text/plain",
-    //   "Content-Type": "application/json",
-    // });
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(
-      JSON.stringify({
-        name: "Bruce",
-        company: "Wayne Corp",
-        occupation: "night vigilante",
-      })
-    );
-  }else if(req.url === '/messages'){
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('messages chunk')
-    res.end()
-  }else{
-    res.statusCode = 404
-    res.end()
+  const items = req.url.split("/");
+
+  if (items[1] === "supes") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    if (items.length === 3) {
+      res.end(JSON.stringify(supes[+items[2]]));
+    } else {
+      res.end(JSON.stringify(supes));
+    }
+  } else if (items[1] === "messages") {
+    res.setHeader("Content-Type", "text/plain");
+    res.write("messages chunk");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
   }
 });
 
