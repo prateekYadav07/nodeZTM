45bc4f729b6fc2e4de2b99d1d097a4aabfb10e72
prateekYadav07 <yadavprateek39@gmail.com>
2023-04-14 16:38:03 +0530

    middlewares

--- EXPRESS-PROJECT/server.js
+++ EXPRESS-PROJECT/server.js
@@ -2,12 +2,41 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const friends = [
+  {
+    id: 0,
+    name: "Sir Issac Newton",
+  },
+  {
+    id: 1,
+    name: "Albert Einstein",
+  },
+];
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}`);
+});
+
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.get("/messages", (req, res) => {
-  res.send("<p><em>Bruce Wayne</em></p>");
+app.get("/friends", (req, res) => {
+  setTimeout(() => {
+    res.json(friends);
+  }, 3000);
+});
+
+// route parameters
+app.get("/friends/:friendId", (req, res) => {
+  const id = req.params.friendId;
+  if (Number(id) !== 0 && !Boolean(Number(id)))
+    res.status(404).send({ error: `Illegal argument: ${req.params.friendId}` });
+  if (!friends[id]) res.status(404).json({ error: "Friend doesn't exist" });
+  res.json(friends[id]);
 });
 
 app.get("/messages", (req, res) => {
